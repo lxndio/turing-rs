321841b46d03221daa5fd81eb3d7f40935083f39
Alexander Korn <alexander@korn-online.org>
2019-04-18 17:06:07 +0200

    A VALUEable member of society

--- src/tape.rs
+++ src/tape.rs
@@ -1,11 +1,15 @@
-pub struct Tape<V: Copy = bool> {
+use std::hash::Hash;
+
+pub struct Tape<Value> {
     /// Tape positions from 0 to infinity
-    positive_tape: Vec<Option<V>>,
+    positive_tape: Vec<Value>,
     /// Tape positions from -1 to -infinity
-    negative_tape: Vec<Option<V>>,
+    negative_tape: Vec<Value>,
     /// The current head position
     head_position: isize
 }
+
+pub type Value<V: Copy + Eq + Hash = bool> = Option<V>;
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum Direction {
@@ -14,28 +18,26 @@ pub enum Direction {
     Right = 1
 }
 
-pub trait X {
-    type Value;
-
+pub trait X<Value> {
     /// Move the Head in any direction, or hold it. Returns the Value on the
     /// Tape, which is at the new position.
-    fn mv(&mut self, direction: Direction) -> Self::Value;
+    fn mv(&mut self, direction: Direction) -> Value;
 
     /// Move the Head left and return the Value that is found there.
-    fn mv_left(&mut self) -> Self::Value;
+    fn mv_left(&mut self) -> Value;
 
     /// Move the Head right and return the Value that is found there.
-    fn mv_right(&mut self) -> Self::Value;
+    fn mv_right(&mut self) -> Value;
 
     /// Don't move the Head and read the Value that is right under it.
-    fn read(&self) -> Self::Value;
+    fn read(&self) -> Value;
 
-    fn write(&mut self, val: Self::Value);
+    fn write(&mut self, val: Value);
 }
 
-impl<V: Copy> Tape<V> {
+impl<Value> Tape<Value> {
     /// Create a new, empty tape
-    pub fn new() -> Tape<V> {
+    pub fn new() -> Tape<Value> {
         Tape {
             positive_tape: Vec::new(),
             negative_tape: Vec::new(),
@@ -44,7 +46,7 @@ impl<V: Copy> Tape<V> {
     }
 
     /// Create a Tape from the values in the slice
-    pub fn tape(tape: Vec<Option<V>>) -> Tape<V> {
+    pub fn tape(tape: Vec<Option<Value>>) -> Tape<Value> {
         Tape {
             positive_tape: tape,
             negative_tape: Vec::new(),
@@ -70,30 +72,28 @@ impl<V: Copy> Tape<V> {
     }
 }
 
-impl<V: Copy> X for Tape<V> {
-    type Value = Option<V>;
-
-    fn mv(&mut self, direction: Direction) -> Option<V> {
+impl X<Value> for Tape<Value> {
+    fn mv(&mut self, direction: Direction) -> Value {
         self.head_position += direction as isize;
         self.read()
     }
 
-    fn mv_left(&mut self) -> Option<V> {
+    fn mv_left(&mut self) -> Value {
         self.head_position -= 1;
         self.read()
     }
 
-    fn mv_right(&mut self) -> Option<V> {
+    fn mv_right(&mut self) -> Value {
         self.head_position += 1;
         self.read()
     }
 
-    fn read(&self) -> Option<V> {
+    fn read(&self) -> Value {
         if self.head_position >= 0 { self.positive_tape[self.head_position as usize] }
         else { self.negative_tape[(self.head_position.abs()-1) as usize] }
     }
 
-    fn write(&mut self, val: Option<V>) {
+    fn write(&mut self, val: Value) {
         if self.head_position >= 0 { self.positive_tape[self.head_position as usize] = val }
         else { self.negative_tape[(self.head_position.abs()-1) as usize] = val }
     }
--- src/turing.rs
+++ src/turing.rs
@@ -1,10 +1,28 @@
-use crate::tape::Tape;
+use std::collections::HashMap;
+use crate::tape::{Direction, Tape, Value, X};
 
-pub type State = usize;
-
-pub struct TuringMachine<V: Copy = bool> {
-    states: Vec<State>,
-    tapes: Vec<Tape<V>>,
+pub struct TuringMachine<Value> {
+    current_state: State,
+    tape: Tape<Value>,
+    transitions: TransitionTable<Value>,
 }
 
+pub type State = usize;
+pub type TransitionTable<Value> = HashMap<(State, Value), (State, Value, Direction)>;
 
+pub trait Transitionable<Value> {
+    fn peek_transition(&self) -> &(State, Value, Direction);
+
+    fn step(&mut self);
+}
+
+impl Transitionable<Value> for TuringMachine<Value> {
+    fn peek_transition(&self) -> &(State, Value, Direction) {
+        //self.transitions.get(&(self.current_state, self.tape.read())).expect("Could not read from transition table")
+        unimplemented!()
+    }
+
+    fn step(&mut self) {
+
+    }
+}
