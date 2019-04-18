ba1798d564fd10396a4fa41807850aae4f197096
Arne Dussin <arne.dussin@live.de>
2019-04-18 22:09:17 +0200

    Make the turing machine createable

--- src/turing_machine.rs
+++ src/turing_machine.rs
@@ -11,9 +11,29 @@ pub struct TuringMachine<V: Tapeable> {
 }
 
 pub trait Transitionable<V> {
+    /// Check what the next transition will be, without actually performing it
     fn peek_transition(&self) -> (State, Option<V>, Direction);
 
+    /// Perform the next transition. Return true, if the turing machine wants
+    /// to continue running, false if it has stopped.
     fn step(&mut self) -> bool;
+}
+
+impl<V: Tapeable> TuringMachine<V> {
+    /// Create a new turing machine with a tape inserted and empty transition function
+    pub fn new(tape: Box<SimpleTape<V>>) -> TuringMachine<V> {
+        TuringMachine {
+            current_state: 0,
+            tape,
+            transitions: HashMap::new()
+        }
+    }
+
+    /// Add a transition to the transition table. If there was already a transition
+    /// registered to the cause, it will be replaced and returned
+    pub fn add_transition(&mut self, cause: (State, Option<V>), effect: (State, Option<V>, Direction)) -> Option<(State, Option<V>, Direction)> {
+        self.transitions.insert(cause, effect)
+    }
 }
 
 impl<V: Tapeable> Transitionable<V> for TuringMachine<V> {
