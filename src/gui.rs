f754bb44ffe79e314f2fdc0602d753f30b423044
Alexander Korn <alexander@korn-online.org>
2019-04-25 14:35:07 +0200

    The twenty buttons from tape

--- src/gui.rs
+++ src/gui.rs
@@ -1,5 +1,5 @@
-use azul::{prelude::*};
-use std::time::Duration;
+use azul::{prelude::*, widgets::{button::Button}};
+
 
 pub struct TuringMachineApp {
     
@@ -7,16 +7,27 @@ pub struct TuringMachineApp {
 
 impl Layout for TuringMachineApp {
     fn layout(&self, _info: LayoutInfo<Self>) -> Dom<Self> {
-        Dom::from_file("gui.xml", &mut XmlComponentMap::default())
+        let mut dom = Dom::from_file("gui.xml", &mut XmlComponentMap::default());
+
+        for i in 0..20 {
+            let button = Button::with_label(i.to_string()).dom()
+                .with_class("tape_element")
+                .with_css_override("left_distance", CssProperty::Left(LayoutLeft::px((10 + i*35) as f32)));
+
+            dom = dom.with_child(button);
+        }
+
+        dom
     }
 }
 
 pub fn gui() {
     let mut app = App::new(TuringMachineApp {}, AppConfig::default()).unwrap();
-    let window = {
+    /*let window = {
         let hot_reloader = css::hot_reload_override_native("gui.css", Duration::from_millis(500));
         app.create_hot_reload_window(WindowCreateOptions::default(), hot_reloader).unwrap()
-    };
-    // let window = app.create_window(WindowCreateOptions::default(), css::override_native("gui.css").unwrap()).unwrap();
+    };*/
+    let css = css::override_native(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/gui.css"))).unwrap();
+    let window = app.create_window(WindowCreateOptions::default(), css).unwrap();
     app.run(window).unwrap();
 }
