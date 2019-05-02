06f98f0af8673deab25dcb6f4745550053821ab2
Alexander Korn <alexander@korn-online.org>
2019-05-02 15:31:58 +0200

    Started adding button callbacks and removed use of a seperate XML file for UI elements (for now)

--- src/gui.rs
+++ src/gui.rs
@@ -1,23 +1,44 @@
-use azul::{prelude::*, widgets::{button::Button}, window::WindowCreateOptions};
+use azul::{prelude::*, window::WindowCreateOptions};
 
 use crate::turing_machine::TuringMachine;
-use crate::tape::{Tape, Tapeable};
+use crate::tape::{Tape};
 
 pub struct TuringMachineApp {
     turing_machine: TuringMachine<bool>,
+    elements_qty: usize,
 }
 
 impl Layout for TuringMachineApp {
     fn layout(&self, _info: LayoutInfo<Self>) -> Dom<Self> {
-        let mut dom = Dom::from_file("gui.xml", &mut XmlComponentMap::default());
+        //let dom = Dom::from_file("gui.xml", &mut XmlComponentMap::default())
+        //    .with_callback(On::MouseUp, Callback(button_pressed));
 
-        let mut cells_dom = self.turing_machine.tape().contents_trim_blanks().iter().enumerate().map(|(i, cell)| NodeData {
+        let dom = Dom::new(NodeType::Div).with_id("main_screen")
+            .with_child(Dom::new(NodeType::Div).with_id("top_bar")
+                .with_child(Dom::label("Start")
+                    .with_class("button")
+                    .with_id("btn_start")
+                    .with_callback(On::MouseUp, Callback(button_pressed)))
+            .with_child(Dom::label("Stop")
+                    .with_class("button")
+                    .with_id("btn_stop")
+                    .with_callback(On::MouseUp, Callback(button_pressed)))
+            .with_child(Dom::label("Step")
+                    .with_class("button")
+                    .with_id("btn_step")
+                    .with_callback(On::MouseUp, Callback(button_pressed)))
+            .with_child(Dom::label("Elements +")
+                    .with_class("button")
+                    .with_id("btn_element_qty_plus")
+                    .with_callback(On::MouseUp, Callback(button_pressed)))
+            .with_child(Dom::label("Elements -")
+                    .with_class("button")
+                    .with_id("btn_element_qty_minus")
+                    .with_callback(On::MouseUp, Callback(button_pressed))));
+
+        let cells_dom = (0..self.elements_qty).map(|i| NodeData {
             node_type: NodeType::Div,
-            classes: vec!["tape_element".into(), match cell {
-                Some(true) => "true_cell".into(),
-                Some(false) => "false_cell".into(),
-                None => "none_cell".into(),
-            }],
+            classes: vec!["tape_element".into(), "none_cell".into()],
             dynamic_css_overrides: vec![("left_distance".into(), CssProperty::Left(LayoutLeft::px((10 + i*49) as f32)))],
             .. Default::default()
         }).collect::<Dom<Self>>();
@@ -28,18 +49,28 @@ impl Layout for TuringMachineApp {
     }
 }
 
+fn button_pressed(app_state: &mut AppState<TuringMachineApp>, event: &mut CallbackInfo<TuringMachineApp>) -> UpdateScreen {
+    if event.target_has_id("btn_element_qty_plus") {
+        app_state.data.modify(|state| state.elements_qty += 1)?;
+    } else if event.target_has_id("btn_element_qty_minus") {
+        app_state.data.modify(|state| state.elements_qty -= 1)?;
+    }
+
+    Redraw
+}
+
 pub fn gui() {
     let vec: Vec<Option<bool>> = vec![Some(true); 35];
+
     let mut app = App::new(TuringMachineApp {
         turing_machine: TuringMachine::new(Box::new(Tape::tape(vec))),
+        elements_qty: 10,
     }, AppConfig::default()).unwrap();
-    /*let window = {
-        let hot_reloader = css::hot_reload_override_native("gui.css", Duration::from_millis(500));
-        app.create_hot_reload_window(WindowCreateOptions::default(), hot_reloader).unwrap()
-    };*/
+
     let css = css::override_native(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/gui.css"))).unwrap();
     let mut window_options = WindowCreateOptions::default();
     window_options.state.title = "Turing RS".to_string();
+
     let window = app.create_window(window_options, css).unwrap();
     app.run(window).unwrap();
 }
